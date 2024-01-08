2d9692338b74c8a5545cc765a977d86233487b94
Adam Dolowy <adamdolowy2016@gmail.com>
2024-01-09 00:01:35 +0100

    Added animals page. Added a few elements on the main page and changed navbar layout.

--- src/main/java/bdbt_bada_project/SpringApplication/AppController.java
+++ src/main/java/bdbt_bada_project/SpringApplication/AppController.java
@@ -15,6 +15,7 @@ public class AppController implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/main").setViewName("main");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/animals").setViewName("animals");
 
         registry.addViewController("/main_admin").setViewName("admin/main_admin");
         registry.addViewController("/main_user").setViewName("user/main_user");
