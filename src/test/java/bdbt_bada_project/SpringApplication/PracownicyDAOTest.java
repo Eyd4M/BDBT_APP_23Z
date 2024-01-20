45555461481d0be81d89b16b6b6f05191c4ab80e
Adam Dolowy <adamdolowy2016@gmail.com>
2024-01-20 15:53:47 +0100

    PracownicyDAOTest

---
+++ src/test/java/bdbt_bada_project/SpringApplication/PracownicyDAOTest.java
@@ -0,0 +1,50 @@
+package bdbt_bada_project.SpringApplication;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PracownicyDAOTest extends Object {
+
+    private PracownicyDAO dao;
+    @BeforeEach
+    void setUp() {
+        DriverManagerDataSource datasource = new DriverManagerDataSource();
+        datasource.setUrl("jdbc:oracle:thin:@194.29.170.4:1521:xe");
+        datasource.setUsername("BDBTGRB07");
+        datasource.setPassword("BDBTGRB07");
+        datasource.setDriverClassName("oracle.jdbc.OracleDriver");
+
+        /* Import JdbcTemplate */
+        dao = new PracownicyDAO(new JdbcTemplate(datasource));
+    }
+
+    @Test
+    void list() {
+        List<Pracownik> listPracownik = dao.list();
+        System.out.print(listPracownik);
+
+        assertTrue(!listPracownik.isEmpty());
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
